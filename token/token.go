ab6c06252b495f4d51ecb16b26010f2605e8fbff
moshed2 <moshe.dego@workiz.com>
2024-04-19 19:16:16 +0300

    add lexer, tokens for language

---
+++ token/token.go
@@ -0,0 +1,47 @@
+package token
+
+const (
+	ILLEGAL = "ILLEAGEL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" //add, foobar, x, y
+	INT   = "INT"   //123513
+
+	//Operator
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
